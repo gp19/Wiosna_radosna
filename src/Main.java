fde86fde85d0a6afa1bca8aea88f3b0d7f651b38
grzeslaw19 <gpawelski1@gmail.com>
2017-10-12 22:51:31 +0200

    Declare variables.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Grzesław on 2017-10-12.
+ */
+public class Main {
+
+    int n; //liczba przedmiotow
+    int J, S; //rozmiary plecakow Jasia i Stasia
+    ArrayList<Thing> things = new ArrayList<>(); //tablica przechowujaca wartosci i rozmiary przedmiotow
+
+    public static void main(String[] args) {
+
+    }
+
+}
