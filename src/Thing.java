c2aa4089c531d09b8dbbba01a832d0c2af51ebfd
grzeslaw19 <gpawelski1@gmail.com>
2017-10-12 22:41:21 +0200

    Create "Thing" class.

---
+++ src/Thing.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Grzesław on 2017-10-12.
+ */
+public class Thing {
+
+    private int mValue;
+    private int mSize;
+
+    public Thing(int value, int size){
+        mValue = value;
+        mSize = size;
+    }
+
+    public int getValue(){
+        return mValue;
+    }
+
+    public int getSize(){
+        return mSize;
+    }
+
+}
