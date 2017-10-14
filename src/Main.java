0e447ee09957e077146c6ee18146e367c37831ea
grzeslaw19 <gpawelski1@gmail.com>
2017-10-14 17:14:02 +0200

    Add functions: "determineClosiongSolution" and "printSolution"

--- src/Main.java
+++ src/Main.java
@@ -10,25 +10,26 @@ class Main {
     private static int J, S; //Rozmiary plecakow Jasia i Stasia
     private static final ArrayList<Item> items = new ArrayList<>(); //Tablica przechowujaca wartosci i rozmiary przedmiotow
     private static final Solution[] solutions = new Solution[2]; //Tablica przechowujaca dwa mozliwe rozwiazania
+    private static Solution closingSolution;
 
     private static final Scanner input = new Scanner(System.in);
+
 
     public static void main(String[] args) {
 
         getBasicData(); //Pobranie liczby przedmiotow oraz rozmiarow plecakow Jasia i Stasia
         getItemsParameters(); //Wywolanie funkcji wczytujacej rozmiary i wartosci rzeczy i zapisanie ich w tablicy things
         sortObjects(); //Sortuj obiekty wedlug ilorazu wartosci i rozmiaru od najwiekszego do najmniejszego
-        calculateSolution(J, S, 0); //Oblicz rozwiazania, pierwsze - zaczynajac od pierwszego plecaka, drugie - zaczynajac od drugiego
-        calculateSolution(S, J, 1); //Oblicz rozwiazania, pierwsze - zaczynajac od pierwszego plecaka, drugie - zaczynajac od drugiego
-
-        System.out.println("\n" + solutions[0].getMaxValue() + "\n" + solutions[0].getFirstKnapsackIndices() + "\n" + solutions[0].getSecondKnapsackIndices());
-        System.out.println("\n\n" + solutions[1].getMaxValue() + "\n" + solutions[1].getFirstKnapsackIndices() + "\n" + solutions[1].getSecondKnapsackIndices());
+        calculateSolution(J, S, 0); //Oblicz rozwiazania, pierwsze - zaczynajac od pierwszego plecaka
+        calculateSolution(S, J, 1); //drugie - zaczynajac od drugiego
+        determineClosingSolution(); //okresl ostateczne rozwiazanie
+        printSolution(); //wydrukuj ostateczne rozwiazanie
 
     }
 
+
     //Funkcja do wczytywania liczby przedmiotow oraz pojemnosci plecakow
     private static void getBasicData() {
-
         int k;
 
         do {
@@ -49,6 +50,7 @@ class Main {
         } while (k == 1); //Jesli zakresy sa bledne, wczytuje jeszcze raz
 
     }
+
 
     //Funkcja do wczytywania wartosci oraz rozmiarow przedmiotow, zapisywanie danych w ArrayList
     private static void getItemsParameters() {
@@ -74,6 +76,7 @@ class Main {
 
         }
     }
+
 
     //Funkcja sortujaca przedmioty wedlug ilorazu wartosci i rozmiaru od najwiekszej do najmmniejszej
     private static void sortObjects() {
@@ -101,6 +104,7 @@ class Main {
         } while (k == 1);
 
     }
+
 
     //Funkcja obliczajaca rozwiazanie dla dwoch plecakow w danej kolejnosci na podstawie ilorazu wartosci i rozmiaru przedmiotow
     private static void calculateSolution(int firstKnapsackCapacity, int secondKnapsackCapacity, int solutionIndex) {
@@ -133,4 +137,34 @@ class Main {
         solutions[solutionIndex] = new Solution(maxValue, firstKnapsackIndices.toString(), secondKnapsackIndices.toString());
 
     }
+
+
+    private static void determineClosingSolution() {
+
+        int maxValue;
+        String firstKnapsackIndices;
+        String secondKnapsackIndices;
+
+        if (solutions[0].getMaxValue() >= solutions[1].getMaxValue()) {
+            maxValue = solutions[0].getMaxValue();
+            firstKnapsackIndices = solutions[0].getFirstKnapsackIndices();
+            secondKnapsackIndices = solutions[0].getSecondKnapsackIndices();
+        } else {
+            maxValue = solutions[1].getMaxValue();
+            firstKnapsackIndices = solutions[1].getSecondKnapsackIndices();
+            secondKnapsackIndices = solutions[1].getFirstKnapsackIndices();
+        }
+
+        closingSolution = new Solution(maxValue,firstKnapsackIndices, secondKnapsackIndices);
+
+    }
+
+    private static void printSolution(){
+
+        System.out.println("\n" + closingSolution.getMaxValue()
+                        + "\n" + closingSolution.getFirstKnapsackIndices()
+                        + "\n" + closingSolution.getSecondKnapsackIndices());
+
+    }
+
 }
